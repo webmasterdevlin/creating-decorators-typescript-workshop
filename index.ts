df4a0f0012bf2b0eb036e7adf7f3cb969ecd03c6
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-02-16 22:07:36 +0100

    initial commit

---
+++ index.ts
@@ -0,0 +1,35 @@
+import { Email } from "./models/Email";
+
+export function sendTS(email: Email): void {
+  // @ts-ignore
+  global.window.open(
+    `mailto:${email.mailto}?subject=${email.subject}&body=${email.body}`
+  );
+}
+
+// @ts-ignore
+export function sendJS(email) {
+  // @ts-ignore
+  global.window.open(
+    `mailto:${email.mailto}?subject=${email.subject}&body=${email.body}`
+  );
+}
+
+export function tryPropsDecorator(): PropertyDecorator {
+  return function(target: Object, propertyKey: string | symbol): void {
+    console.log("tryPropsDecorator target::", target);
+  };
+}
+
+export function tryMethodDecorator(): MethodDecorator {
+  return function(
+    target: Object,
+    propertyKey: string | symbol,
+    descriptor: PropertyDescriptor
+  ): void {
+    console.log("tryMethodDecorator target::", target);
+    console.log("propertyKey::", propertyKey);
+    console.log("descriptor::", descriptor);
+    alert(`${String(propertyKey)} method is called`);
+  };
+}
---
+++ models/Email.ts
@@ -0,0 +1,5 @@
+export interface Email {
+    mailto: string;
+    subject: string;
+    body: string;
+}
